607a41316ea6bd523c2df1851ed3d3b7b4782ce2
AdrianMSS <sesamaua@gmail.com>
2019-07-29 15:16:20 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-4k2ncy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-4k2ncy)
---
+++ index.js
@@ -0,0 +1,17 @@
+// Import stylesheets
+import './style.css';
+import './css/sb-admin-2.css';
+import './vendor/fontawesome-free/css/all.min.css';
+import './css/font-awesome.css';
+import 'fontawesome';
+
+
+import './vendor/jquery/jquery.min.js';
+import './vendor/bootstrap/js/bootstrap.bundle.min.js';
+import './vendor/chart.js/Chart.min.js';
+import './js/demo/chart-area-demo.js';
+import './js/demo/chart-pie-demo.js';
+import './js/demo/chart-pie-demo.js';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
